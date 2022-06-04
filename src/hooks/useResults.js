f0753ff3e4db88c781684bd76a2f2ddbda8b1411
ashuneema28 <ashu.neema28@gmail.com>
2022-06-03 23:09:57 -0600

    Issue FIX

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -6,13 +6,12 @@ export default () => {
   const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async searchTerm => {
-    console.log('Hi there!');
     try {
       const response = await yelp.get('/search', {
         params: {
           limit: 50,
           term: searchTerm,
-          location: 'san jose'
+          location: 'Utah'
         }
       });
       setResults(response.data.businesses);
